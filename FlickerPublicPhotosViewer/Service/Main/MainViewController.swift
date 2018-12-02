0a6fefb576124a9e9bf727edc455fdec605423d3
JungMin Ahn <jungminahn.minsone@gmail.com>
2018-12-02 19:55:37 +0900

    Main화면에 CollectionView 추가

--- FlickerPublicPhotosViewer/Service/Main/MainViewController.swift
+++ FlickerPublicPhotosViewer/Service/Main/MainViewController.swift
@@ -10,11 +10,14 @@ import UIKit
 import ReactorKit
 import RxSwift
 
+final class MainViewController: UIViewController {
 
-class MainViewController: UIViewController {
-
+    @IBOutlet private(set) weak var collectionView: UICollectionView!
+    
     let reactor = MainViewReactor()
     let bag = DisposeBag()
+    
+    private var collectionShim: MainViewCollectionViewShim?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,7 +28,7 @@ class MainViewController: UIViewController {
     }
     
     private func setup() {
-        
+        self.collectionShim = .init(collectionView: collectionView)
     }
     
     private func bindState() {
@@ -59,3 +62,46 @@ class MainViewController: UIViewController {
     }
 }
 
+
+private class MainViewCollectionViewShim: NSObject, UICollectionViewDelegate, UICollectionViewDataSource {
+    private var dataSource: [URL?] = []
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MainViewThumbnailImageCollectionViewCell", for: indexPath) as? MainViewThumbnailImageCollectionViewCell {
+            return cell
+        }
+        return MainViewThumbnailImageCollectionViewCell()
+    }
+    
+    weak var collectionView: UICollectionView?
+    init(collectionView: UICollectionView) {
+        self.collectionView = collectionView
+        super.init()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+}
+
+private class MainViewThumbnailImageCollectionViewCell: UICollectionViewCell {
+    @IBOutlet private(set) weak var imageView: UIImageView!
+
+    private var imageURL: URL? = nil
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        imageView.image = nil
+        imageURL = nil
+    }
+    
+    func setImageURL(_ url: URL?) {
+        
+    }
+}
