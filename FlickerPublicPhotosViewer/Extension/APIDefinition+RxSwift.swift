5fbc13f8fa4f505f58e495e044725741b36c1846
JungMin Ahn <jungminahn.minsone@gmail.com>
2018-12-03 21:29:43 +0900

    modify generics code

--- FlickerPublicPhotosViewer/Extension/APIDefinition+RxSwift.swift
+++ FlickerPublicPhotosViewer/Extension/APIDefinition+RxSwift.swift
@@ -10,14 +10,12 @@ import FoundationExtension
 import RxSwift
 
 extension APIDefinition {
-    func requestObservable<R: ResponseDataTypeProtocol>() -> Observable<R> {
+    func requestObservable() -> Observable<Response> {
         return Observable.create { observer -> Disposable in
             self.request(completion: { (result) in
                 switch result {
                 case .value(let v):
-                    if let v = v as? R {
-                        observer.onNext(v as R)
-                    }
+                    observer.onNext(v)
                     observer.onCompleted()
                 case .error(let e):
                     observer.onError(e)
